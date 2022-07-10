dcc600fd3c119178efc1dfd93ca4b96ca835ffd4
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-07-10 17:32:37 +0300

    add linting

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,4 +1,4 @@
-const { createWebpackConfigAsync } = require("expo-yarn-workspaces/webpack");
+const {createWebpackConfigAsync} = require('expo-yarn-workspaces/webpack');
 
 module.exports = async function (env, argv) {
   const config = await createWebpackConfigAsync(env, argv);
